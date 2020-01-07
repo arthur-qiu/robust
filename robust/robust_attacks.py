672ddfdab6ec798318df2cae05996b71ffa09f7d
邱浩楠 <Arthur>
2020-01-07 23:49:27 +0800

    add mixup

--- robust/robust_attacks.py
+++ robust/robust_attacks.py
@@ -75,6 +75,44 @@ class PGD(nn.Module):
 
         return adv_bx
 
+class MIX_PGD(nn.Module):
+    def __init__(self, epsilon, num_steps, step_size, grad_sign=True):
+        super().__init__()
+        self.epsilon = epsilon
+        self.num_steps = num_steps
+        self.step_size = step_size
+        self.grad_sign = grad_sign
+
+    def forward(self, model, bx, by, by_a, by_b, lam):
+        """
+        :param model: the classifier's forward method
+        :param bx: batch of images
+        :param by: true labels
+        :return: perturbed batch of images
+        """
+        adv_bx = bx.detach().clone()
+        adv_bx += torch.zeros_like(adv_bx).uniform_(-self.epsilon, self.epsilon)
+        adv_bx = adv_bx.clamp(0, 1)
+
+        for i in range(self.num_steps):
+            adv_bx.requires_grad_()
+            with torch.enable_grad():
+                logits = model(adv_bx)
+                if len(logits) == 2:
+                    logits = logits[1]
+                loss = lam * F.cross_entropy(logits, by_a, reduction='sum') + (1 - lam) * F.cross_entropy(logits, by_b, reduction='sum')
+            grad = torch.autograd.grad(loss, adv_bx, only_inputs=True)[0]
+
+            if self.grad_sign:
+                adv_bx = adv_bx.detach() + self.step_size * torch.sign(grad.detach())
+            else:
+                grad = normalize_l2(grad.detach())
+                adv_bx = adv_bx.detach() + self.step_size * grad
+
+            adv_bx = torch.min(torch.max(adv_bx, bx - self.epsilon), bx + self.epsilon).clamp(0, 1)
+
+        return adv_bx
+
 #
 # class PGDTarget(nn.Module):
 #     def __init__(self, epsilon, num_steps, step_size, grad_sign=True):
